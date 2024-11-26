2a7e8af7a47e71deb1849a73282e22445453310b
intisar zeynedin <intisar9800@gmail.com>
2024-11-26 23:36:33 +0300

    changed some code part

--- db_connection.php
+++ db_connection.php
@@ -1,13 +1,13 @@
 <?php
 $servername = "localhost";
-$username = "root"; // Replace with your database username  
-$password = ""; // Replace with your database password
-$dbname = "hoteldb"; // Replace with your database name
+$username = "root";   
+$password = ""; 
+$dbname = "hoteldb"; 
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
